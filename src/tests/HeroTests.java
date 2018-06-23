15a16f8edf3af62af67ce6207df34127a3fca9aa
Denis <deviol@abv.bg>
2018-06-23 18:32:52 +0300

    created tests for HeroDirection

--- src/tests/HeroTests.java
+++ src/tests/HeroTests.java
@@ -1,6 +1,8 @@
 package tests;
 
 import game.Hero;
+import game.HeroDirection;
+import game.HeroPosition;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,10 +10,13 @@ import org.junit.Test;
 public class HeroTests {
 
     private Hero hero;
+    private HeroPosition heroPosition;
 
     @Before
     public void initializeHero() {
-        hero = new Hero(50);
+        int randomMaxHealth = 50;
+        hero = new Hero(randomMaxHealth);
+        heroPosition = new HeroPosition();
     }
 
     @Test
@@ -24,5 +29,19 @@ public class HeroTests {
     public void testDecreaseHealthWithCaseSumLessThanZeroShouldAssignZero() {
         hero.decreaseHealthWith(51);
         Assert.assertTrue(hero.getHealth() == 0);
+    }
+
+    @Test
+    public void testUpdateHeroPositionCaseLEFTShouldDecrementColumnIndex() {
+        int expectedColumnIndex = HeroPosition.DEFAULT_INDEX_OF_HERO_POSITION - 1;
+        heroPosition.updateHeroPosition(HeroDirection.LEFT);
+        Assert.assertTrue(heroPosition.getColumnIndex() == expectedColumnIndex);
+    }
+
+    @Test
+    public void testUpdateHeroPositionDefaultCaseShouldIncrementColumnIndex() {
+        int expectedColumnIndex = HeroPosition.DEFAULT_INDEX_OF_HERO_POSITION + 1;
+        heroPosition.updateHeroPosition(HeroDirection.RIGHT);
+        Assert.assertTrue(heroPosition.getColumnIndex() == expectedColumnIndex);
     }
 }
