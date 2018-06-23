194b9faa9954859ebfa1682d0fe7ec2f569460ee
Denis <deviol@abv.bg>
2018-06-23 18:03:22 +0300

    made class FieldElement and enum FieldElementStatus

---
+++ src/game/FieldElement.java
@@ -0,0 +1,31 @@
+package game;
+
+public class FieldElement {
+    private FieldElementStatus status;
+    private String code;
+
+    public FieldElement(String code) {
+        status = FieldElementStatus.HIDDEN;
+        this.code = code;
+    }
+
+    public FieldElement() {
+        status = FieldElementStatus.HIDDEN;
+        code = "empty";
+    }
+    public FieldElementStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(FieldElementStatus status) {
+        this.status = status;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
