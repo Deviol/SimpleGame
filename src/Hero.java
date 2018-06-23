99473070e1eed497b2c524604fe756ce1a2f1439
Denis <deviol@abv.bg>
2018-06-23 17:05:54 +0300

    created a class Hero

---
+++ src/Hero.java
@@ -0,0 +1,33 @@
+public class Hero {
+    private int health;
+    private int maxHealth;
+
+    public Hero(int maxHealth) {
+        this.maxHealth = maxHealth;
+        health = maxHealth;
+    }
+
+    public void increaseHealthWith(int health) {
+        if(health < 0) {
+            health = 0;
+        }
+        if(this.health + health < maxHealth) {
+            this.health += health;
+        }
+        else {
+            this.health = maxHealth;
+        }
+    }
+
+    public void decreaseHealthWith(int health) {
+        if(health < 0) {
+            health = 0;
+        }
+        if(this.health - health >= 0) {
+            this.health -= health;
+        }
+        else {
+            this.health = 0;
+        }
+    }
+}
