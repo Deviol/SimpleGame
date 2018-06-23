890fd723be4d10dcb216b971473c6cfaf612421c
Denis <deviol@abv.bg>
2018-06-23 20:34:06 +0300

    fixed HeroStepOutOfGameFieldBoundsException and added a new method in DefaultLevel

--- src/game/DefaultLevel.java
+++ src/game/DefaultLevel.java
@@ -1,5 +1,22 @@
 package game;
 
 public class DefaultLevel {
-    
+
+    private GameField gameField;
+    private Hero hero;
+    private LevelStatus levelStatus;
+    private HeroPosition heroPosition;
+
+    public DefaultLevel(Hero hero) {
+        this.gameField = new GameField();
+        this.hero = hero;
+        levelStatus = LevelStatus.RUNNING;
+        heroPosition = new HeroPosition();
+    }
+
+    private void makeOldStepUsed() {
+        int heroRow = heroPosition.getRowIndex();
+        int heroCol = heroPosition.getColumnIndex();
+        gameField.setNewFieldElementCode(heroRow, heroCol, "used");
+    }
 }
