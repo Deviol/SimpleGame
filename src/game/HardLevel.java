285957b631b41161927b559e604fb1d809b30ff8
Denis <deviol@abv.bg>
2018-06-24 14:37:17 +0300

    added moving{left, right, up, down} functions in HardLevel class

--- src/game/HardLevel.java
+++ src/game/HardLevel.java
@@ -5,9 +5,9 @@ public class HardLevel extends DefaultLevel{
     private Hero hero;
     private HeroDirection firstForbiddenDirection;
     private HeroDirection secondForbiddenDirection;
-    int numberOfAllowedSteps;
-    int healthCostForPlaying;
-
+    private int numberOfAllowedSteps;
+    private int healthCostForPlaying;
+    private int additionalHealthForEachStep;
     public HardLevel(Hero hero, HeroDirection firstForbiddenDirection,
                  HeroDirection secondForbiddenDirection,
                  int numberOfAllowedSteps, int healthCostForPlaying) {
@@ -16,6 +16,51 @@ public class HardLevel extends DefaultLevel{
         this.secondForbiddenDirection = secondForbiddenDirection;
         this.numberOfAllowedSteps = numberOfAllowedSteps;
         this.healthCostForPlaying = healthCostForPlaying;
+        additionalHealthForEachStep = 0;
     }
-    
+
+    public void movingLeft() throws HeroStepOutOfGameFieldBoundsException,
+            ForbiddenDirectionException {
+        boolean isForbiddenDirection = isForbiddenDirection(HeroDirection.LEFT);
+        if(isForbiddenDirection) {
+            throw new ForbiddenDirectionException();
+        }
+        super.movingLeft();
+    }
+
+    public void movingRight() throws HeroStepOutOfGameFieldBoundsException,
+            ForbiddenDirectionException {
+        boolean isForbiddenDirection = isForbiddenDirection(HeroDirection.RIGHT);
+        if(isForbiddenDirection ) {
+            throw new ForbiddenDirectionException();
+        }
+        super.movingRight();
+    }
+
+    public void movingUp() throws HeroStepOutOfGameFieldBoundsException,
+            ForbiddenDirectionException {
+        boolean isForbiddenDirection = isForbiddenDirection(HeroDirection.UP);
+        if(isForbiddenDirection ) {
+            throw new ForbiddenDirectionException();
+        }
+        super.movingUp();
+    }
+
+    public void movingDown() throws HeroStepOutOfGameFieldBoundsException,
+            ForbiddenDirectionException {
+        boolean isForbiddenDirection = isForbiddenDirection(HeroDirection.DOWN);
+        if(isForbiddenDirection ) {
+            throw new ForbiddenDirectionException();
+        }
+        super.movingDown();
+    }
+
+    public void activateSpell() throws NoSpellFoundException {
+        super.activateSpell();
+    }
+
+    private boolean isForbiddenDirection(HeroDirection direction) {
+        return direction == firstForbiddenDirection ||
+            direction == secondForbiddenDirection;
+    }
 }
